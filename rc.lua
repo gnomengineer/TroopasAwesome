3fb7e2960c51666f61e5b37fedf75ace1fd51c89
Troopa <troopa.der.pate@gmail.com>
2012-05-15 21:03:23 +0200

    some widgets from vicious initialized

--- rc.lua
+++ rc.lua
@@ -33,8 +33,46 @@ end
 -- }}
 
 -- {{ widgets
+-- { separator
+separator = widget({ type = "imagebox" })
+--separator.image = image(beautiful.separator) --need a new symbol in the theme
+-- }
+
+-- { textclock
+clock = awful.widget.textclock({ align = "right" })
+-- }
+
+-- { systray
+systray = widget({ type = "systray" })
+-- }
+
+-- { cpu usage and temperature
+cpuicon = widget({ type = "imagebox" })
+--cpuicon.image = image(beautiful.cpu) --need a new symbol in the theme
+-- initialize
+cpugraph = awful.widget.graph()
+tempearture = widget({ type = "textbox" })
+-- graph properties
+cpugraph:set_width(40):set_height(15)
+cpugraph:set_background_color(beautiful.gbgcolor) --color have to be defined in the theme
+cpugraph:set_gradient_angle(0):set_gradient_colors(beautiful.gfgcolor)
+-- register
+vicious.register(cpugraph, vicious.widgets.cpu, "$1")
+vicious.register(temperature, vicious.widgets.thermal, "$1", 42, "thermal_zone0")
+-- }
+
+-- { memory usage
+memicon = widget({ type = "imagebox" })
+--memicon.image = image(beautiful.memory) --need a new symbol in the theme
+-- initialize
+memory
+-- { upload/download rate
+-- { launcher for power off etc
+-- { menu launcher 
+
 
 -- }}
 
 -- {{ wibox
-
+for s = 1, screen.count() do
+	
