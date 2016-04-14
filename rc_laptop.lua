a035e3149c357e2cd8e873dc1297bc77d023e390
troopa <troopa.der.pate@gmail>
2016-04-14 08:16:24 +0200

    renamed 2 menu items and changed screen lock application

--- rc_laptop.lua
+++ rc_laptop.lua
@@ -114,9 +114,9 @@ office = { { "editor", "gedit"},
 mainmenu = awful.menu({ items = { { "terminal", terminal },
 								  { "browser", "chromium" },
 								  { "office", office },
-								  { "developing", developer},
+								  { "r&d", developer},
                                   { "maths", mathematics},
-                                  { "multimedia", multimedia }
+                                  { "media", multimedia }
 								}
 					 })
 
@@ -317,7 +317,7 @@ globalkeys = awful.util.table.join(
     awful.key({ superkey, "Shift" }, "g", function () awful.util.spawn("gimp") end),
     awful.key({ superkey }, "i", function () awful.util.spawn(terminal .. " -e screen irssi") end),
     awful.key({ superkey, "Shift" }, "l", function () awful.util.spawn("texmaker") end),
-    awful.key({ superkey }, "l", function () awful.util.spawn("xtrlock") end),
+    awful.key({ superkey }, "l", function () awful.util.spawn("slock") end),
 	--key bindings for easy access to the layouts
 	awful.key({ superkey, }, "space", function () awful.layout.inc(layouts, 1) end),
 	awful.key({ superkey, "Shift"}, "space", function () awful.layout.inc(layouts, -1) end),
